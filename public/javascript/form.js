00c8efb58da69630243f9d0d219dffc92e5e5c09
Vinicin1101 <vserafim_o@outlook.com>
2022-04-21 22:51:40 -0300

    base

---
+++ public/javascript/form.js
@@ -0,0 +1,60 @@
+$('document').ready(function () {
+    $('button').click(function () {
+
+        let prod = $('#nome_produto').val();
+        let num_caixas = $('#num_caixas').val();
+        let nomeForn = $('#fornecedor').val();
+        let dataForn = $('#data_forn').val();
+        let valor = Number($('#valor').val());
+
+        if (prod.trim() == '' || num_caixas == '' || nomeForn.trim() == '' || dataForn == '' || valor == '') {
+            alert('Preencha todos os campos!');
+        } else {
+            let coca = jsonCoca(prod, num_caixas, nomeForn, dataForn, valor);
+
+            $.ajax({
+                url: 'http://localhost:3000/insert',
+                type: 'POST',
+                data: coca,
+                success: function (data) {
+                    alert('Coca adicionada com sucesso!');
+                    $('#nome_produto').val('');
+                    $('#num_caixas').val('');
+                    $('#fornecedor').val('');
+                    $('#data_forn').val('');
+                    $('#valor').val('');
+
+                    console.log(data);
+                }
+            });
+        }
+    });
+
+    function jsonCoca(prod, num_caixas, nomeForn, dataForn, valor) {
+        let coca = {
+            prod: prod,
+            qtd: {
+                caixa: num_caixas,
+                unidade: num_caixas * 12
+            },
+            preco: {
+                unidade: (((valor / num_caixas) / 12) * 2).toFixed(2),
+                caixa: ((valor / num_caixas) * 2).toFixed(2)
+            },
+            ultima_encomenda: {
+                data: dataForn,
+                fornecedor: nomeForn,
+                quantidade: {
+                    caixa: num_caixas,
+                },
+                valor: {
+                    total: valor,
+                    caixa: valor / num_caixas,
+                }
+            }
+        }
+
+        return coca;
+
+    }
+});
---
+++ routes/produtos.js
@@ -0,0 +1,92 @@
+var express = require('express');
+var router = express.Router();
+
+router.post('/produtos/insert', function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', "*");
+    res.header('Access-Control-Allow-Methods', 'POST');
+    res.header("Access-Control-Allow-Headers", "accept, content-type");
+    res.header("Access-Control-Max-Age", "1728000");
+    res.status(200);
+    res.send(req.body);
+});
+
+router.get('/produtos/list', function (req, res, next) {
+    var produtos = [
+        {
+            "id": 1,
+            "nome": "Coca-Cola",
+            "qtd": {
+                "caixa": 12,
+                "unidade": 144
+            },
+            "preco": {
+                "unidade": 2.50,
+                "caixa": 5.00
+            },
+            "ultima_encomenda": {
+                "data": "01/01/2018",
+                "fornecedor": "Coca-Cola",
+                "quantidade": {
+                    "caixa": 12,
+                },
+                "valor": {
+                    "total": 60.00,
+                    "caixa": 5.00
+                }
+            }
+        },
+        {
+            "id": 2,
+            "nome": "Fanta",
+            "qtd": {
+                "caixa": 12,
+                "unidade": 144
+            },
+            "preco": {
+                "unidade": 2.50,
+                "caixa": 5.00
+            },
+            "ultima_encomenda": {
+                "data": "01/01/2018",
+                "fornecedor": "Fanta",
+                "quantidade": {
+                    "caixa": 12,
+                },
+                "valor": {
+                    "total": 60.00,
+                    "caixa": 5.00
+                }
+            }
+        },
+        {
+            "id": 3,
+            "nome": "Sprite",
+            "qtd": {
+                "caixa": 12,
+                "unidade": 144
+            },
+            "preco": {
+                "unidade": 2.50,
+                "caixa": 5.00
+            },
+            "ultima_encomenda": {
+                "data": "01/01/2018",
+                "fornecedor": "Sprite",
+                "quantidade": {
+                    "caixa": 12,
+                },
+                "valor": {
+                    "total": 60.00,
+                    "caixa": 5.00
+                }
+            }
+        },
+    ];
+    res.render("list", {
+        title: "Lista de produtos",
+        produtos: produtos,
+        nome_produto: 'TEste'
+    });
+})
+
+module.exports = router;
